b017ac3bc826cf63baacfdbce1b523f9b6fd969e
Ricardo Marcano <Ricardo Marcano@RicardoMarcano>
2015-06-19 01:09:41 -0430

    Inicio

---
+++ src/Clases/Jugador.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Ricardo Marcano
+ */
+public class Jugador {
+    String nombre;
+    String apellido;
+    int puntos;
+    int x;
+    int y;
+    JLabel bomberman = new JLabel();
+
+    public Jugador(String nombre, String apellido, int x, int y,String color) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        puntos=0;
+        this.x = x;
+        this.y = y;
+        bomberman.setBounds(x,y,43,43);
+    }
+
+    public void setBomberman(JLabel bomberman) {
+        this.bomberman = bomberman;
+    }
+
+    public JLabel getBomberman() {
+        return bomberman;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+    
+}
